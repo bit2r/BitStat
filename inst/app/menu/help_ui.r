a8ac567fdefb5f27ba67afb4e862f3369d22c029
Choonghyun Ryu <choonghyun.ryu@gmail.com>
2021-10-22 17:28:15 +0900

    Reorg help page for i18n

--- inst/app/menu/help_ui.r
+++ inst/app/menu/help_ui.r
@@ -1,18 +1,39 @@
 ## reference https://stackoverflow.com/questions/24875943/display-html-file-in-shiny-app
 addResourcePath("tmpuser", getwd())
 
-output$date_prepare <- renderUI({
+help_language <- reactive({
+  get("language", envir = .BitStatEnv)
+}) 
+
+output$data_prepare <- renderUI({
   tags$iframe(
     seamless = "seamless",
-    src = "tmpuser/help/date_prepare.html",
+    src = glue::glue("tmpuser/help/data_prepare_{help_language()}.html"),
     width = "100%",
     height = "100%"
   )
 })
 
-# output$date_prepare <- renderUI({
-#   includeHTML("help/date_prepare.html")
-# })
+
+output$data_disgnose <- renderUI({
+  tags$iframe(
+    seamless = "seamless",
+    src = glue::glue("tmpuser/help/data_diagnose_{help_language()}.html"),
+    width = "100%",
+    height = "100%"
+  )
+})
+
+
+output$data_trans <- renderUI({
+  tags$iframe(
+    seamless = "seamless",
+    src = glue::glue("tmpuser/help/data_trans_{help_language()}.html"),
+    width = "100%",
+    height = "100%"
+  )
+})
+
 
 output$ui_help <- renderUI({
   tagList(
@@ -23,13 +44,15 @@ output$ui_help <- renderUI({
         tabsetPanel(
           tabPanel(
             title = translate("데이터 준비"),
-            htmlOutput("date_prepare", style = "height: 700px;")
+            htmlOutput("data_prepare", style = "height: 700px;")
           ),
           tabPanel(
-            title = translate("데이터 진단")
+            title = translate("데이터 진단"),
+            htmlOutput("data_disgnose", style = "height: 700px;")            
           ),
           tabPanel(
-            title = translate("데이터 변환")
+            title = translate("데이터 변환"),
+            htmlOutput("data_trans", style = "height: 700px;")            
           )          
         )
       ),
