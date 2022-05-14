b6dd38860d61f68fe42bde71fd261fc9b02df170
Choonghyun Ryu <choonghyun.ryu@gmail.com>
2022-05-14 23:05:41 +0900

    fix translation.csv, etc

--- R/zzz.R
+++ R/zzz.R
@@ -7,8 +7,13 @@
   trans_csv <- file.path(system.file(package = "BitStat"), 
                          "translation", trans_file)
   
-  translation <- readr::read_csv(trans_csv, show_col_types = FALSE,
-                                 locale = readr::locale(encoding = "UTF-8"))
+  translation <- readr::read_csv(
+    trans_csv, 
+    col_types = "cc",
+    locale = readr::locale(encoding = "UTF-8")
+  ) %>% 
+    suppressWarnings()
+  
   assign("translation", translation, envir = .BitStatEnv)
   
   assign(".BitStatEnv", .BitStatEnv, envir = .GlobalEnv)
